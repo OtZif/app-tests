99295a1cc987426b90a8bc8c8ec7cecb2919c53c
Zarif <zarif.otambekov@saltpepper.ru>
2019-11-12 13:54:24 +0300

    init

---
+++ src/reducers/reducer.jsx
@@ -0,0 +1,31 @@
+// state = {
+//   admin: false,
+//   tests: [
+//     {
+//       name: "Тест на внимательность!",
+//       questions: [
+//         {
+//           question: 'question 1?',
+//           answers:[
+//             'answer 1',
+//             'answer 2',
+//             'answer 3'
+//           ]
+//         }
+//       ]
+//     },
+//     {
+//       name: "Тест на bla bla!",
+//       questions: [
+//         {
+//           question: 'question 1?',
+//           answers:[
+//             'answer 1',
+//             'answer 2',
+//             'answer 3'
+//           ]
+//         }
+//       ]
+//     }
+//   ]
+// };
